128e2c1f5e653e329dad13b687a7978702bac1b7
unknown <aldushin.s@toredo.ru>
2024-03-06 03:55:28 +0300

    menu

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const rightImage = document.querySelector('.banner__container-right__block-image
 const bottomLeftImages = document.querySelectorAll('.banner__container-bottom__left-block__item-image');
 const bottomRightImage = document.querySelector('.banner__container-bottom__right-block__image');
 
+const menuButton = document.querySelector('.navbar__right-button');
+const mobileMenu = document.querySelector('.navbar__mobile');
+
+
+
 leftImages.forEach((image) => {
   image.addEventListener('click', (e) => {
     const index = e.target.getAttribute('data-index');
@@ -16,4 +21,29 @@ bottomLeftImages.forEach((image) => {
     const index = e.target.getAttribute('data-index');
     bottomRightImage.src = e.target.src;
   });
+});
+
+menuButton.addEventListener('click', () => {
+  if (mobileMenu.classList.contains('active')) {
+    mobileMenu.classList.remove('active');
+    menuButton.textContent = 'Меню';
+  } else {
+    mobileMenu.classList.add('active');
+    menuButton.textContent = 'Закрыть';
+  }
+});
+
+const navbarMobileItemContainer = document.querySelector('.navbar__mobile-item__container');
+const navbarMobileItemAboutTitle = document.querySelector('.navbar__mobile-item__about__title');
+const navbarMobileContainer = document.querySelector('.navbar__mobile-container');
+const navbarMobileContainerAbout = document.querySelector('.navbar__mobile-container__about');
+
+navbarMobileItemContainer.addEventListener('click', () => {
+  navbarMobileContainer.style.display = 'none';
+  navbarMobileContainerAbout.style.display = 'block';
+});
+
+navbarMobileItemAboutTitle.addEventListener('click', () => {
+  navbarMobileContainer.style.display = 'block';
+  navbarMobileContainerAbout.style.display = 'none';
 });
