904eaf955c030831f19545ab755e3219d37cec7f
unknown <aldushin.s@toredo.ru>
2024-03-07 07:16:29 +0300

    slider

--- app.js
+++ app.js
@@ -47,3 +47,65 @@ navbarMobileItemAboutTitle.addEventListener('click', () => {
   navbarMobileContainer.style.display = 'block';
   navbarMobileContainerAbout.style.display = 'none';
 });
+
+const slider = document.querySelector('.slider');
+const sliderWrapper = slider.querySelector('.slider__wrapper');
+const sliderItems = slider.querySelectorAll('.slider__wrapper-item');
+const sliderNav = slider.querySelector('.slider__controll-nav');
+const sliderNavItems = slider.querySelectorAll('.slider__controll-nav__item');
+const sliderArrows = slider.querySelectorAll('.slider__controll-arrow');
+
+let currentSlide = 0;
+const marginLeft = 24;
+
+const slideWidth = sliderItems[0].offsetWidth;
+const slidesCount = sliderItems.length;
+
+sliderWrapper.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+
+sliderNavItems.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    currentSlide = index;
+    sliderWrapper.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+
+    sliderNavItems.forEach(item => {
+      item.classList.remove('active');
+    });
+
+    item.classList.add('active');
+  });
+});
+
+sliderArrows.forEach(arrow => {
+  arrow.addEventListener('click', () => {
+    if (arrow.classList.contains('slider-arrow--left')) {
+      currentSlide--;
+    } else if (arrow.classList.contains('slider-arrow--right')) {
+      if (currentSlide < slidesCount - 5) {
+        currentSlide++;
+      } else {
+        currentSlide = 0;
+      }
+    }
+
+    if (currentSlide < 0) {
+      currentSlide = slidesCount - 5;
+    } else if (currentSlide >= slidesCount) {
+      currentSlide = 0;
+    }
+
+    if (currentSlide >= slidesCount - 1) {
+      sliderArrows[1].classList.add('disabled');
+    } else {
+      sliderArrows[1].classList.remove('disabled');
+    }
+
+    sliderWrapper.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+
+    sliderNavItems.forEach(item => {
+      item.classList.remove('active');
+    });
+
+    sliderNavItems[currentSlide].classList.add('active');
+  });
+});
